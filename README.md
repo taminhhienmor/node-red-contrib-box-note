92124f1dc54b3834c9644771d1c9e420fca597ed
taminhhienmor <56866116+taminhhienmor@users.noreply.github.com>
2020-02-28 20:01:36 +0900

    version 0.05

--- README.md
+++ README.md
@@ -5,14 +5,14 @@ This is a node-red node for performing http(s) requests that use [Request](https
 run npm -g install node-red-contrib-box-note
 
 ## Features
-Upload box note<br/>
+Create new note box with template
 
 # Get Key
 Follow to guideline: https://github.com/taminhhienmor/node-red-contrib-box-note/blob/master/guidelineGetKey.docx
 
 ## Example node
 ``` node
-[{"id":"4efcdd7c.de8da4","type":"box out","z":"c0cc4cd.3edecb","box":"","filename":"testfile.boxnote","localFilename":"","name":"testfile","x":530,"y":220,"wires":[["ad4d1b34.119b78"]]},{"id":"b78eb46d.c569b8","type":"inject","z":"c0cc4cd.3edecb","name":"","topic":"","payload":"","payloadType":"date","repeat":"","crontab":"","once":false,"onceDelay":0.1,"x":260,"y":220,"wires":[["4efcdd7c.de8da4"]]},{"id":"ad4d1b34.119b78","type":"debug","z":"c0cc4cd.3edecb","name":"","active":true,"tosidebar":true,"console":false,"tostatus":false,"complete":"false","x":790,"y":220,"wires":[]}]
+[{"id":"b78eb46d.c569b8","type":"inject","z":"c0cc4cd.3edecb","name":"","topic":"","payload":"","payloadType":"date","repeat":"","crontab":"","once":false,"onceDelay":0.1,"x":240,"y":500,"wires":[["eb9df988.5ddbc8"]]},{"id":"ad4d1b34.119b78","type":"debug","z":"c0cc4cd.3edecb","name":"","active":true,"tosidebar":true,"console":false,"tostatus":false,"complete":"false","x":770,"y":500,"wires":[]},{"id":"eb9df988.5ddbc8","type":"box","z":"c0cc4cd.3edecb","box":"","filename":"testFile.boxnote","localFilename":"","name":"testFile","urlTemplate":"https://app.box.com/notes/574880606388","urlFolder":"https://app.box.com/folder/86032894587","x":520,"y":500,"wires":[["ad4d1b34.119b78"]]}]
 ```
 
 ## Reference
--- source/createNewBox.js
+++ source/createNewBox.js
@@ -333,63 +333,120 @@ module.exports = function(RED) {
             if (!localFilename && typeof msg.payload === "undefined") {
                 return;
             }
-            node.box.resolvePath(path, function(err, parent_id) {
-                if (err) {
-                    node.error(RED._("box.error.path-resolve-failed",{err:err.toString()}),msg);
-                    node.status({fill:"red",shape:"ring",text:"box.status.failed"});
-                    return;
-                }
-                node.status({fill:"blue",shape:"dot",text:"box.status.uploading"});
-                var r = node.box.request({
-                    method: 'POST',
-                    url: 'https://upload.box.com/api/2.0/files/content',
-                }, function(err, data) {
+
+            var urlFolder = n.urlFolder || msg.urlFolder
+            var folder = urlFolder ? urlFolder.split("/").pop() : 0
+
+            var urlTemplate = n.urlTemplate || msg.urlTemplate
+            if(!urlTemplate || typeof(urlTemplate) == "undefined") {
+                node.box.resolvePath(path, function(err, parent_id) {
                     if (err) {
-                        if (data && data.status === 409 &&
-                            data.context_info && data.context_info.conflicts) {
-                            // existing file, attempt to overwrite it
-                            node.status({fill:"blue",shape:"dot",text:"box.status.overwriting"});
-                            var r = node.box.request({
-                                method: 'POST',
-                                url: 'https://upload.box.com/api/2.0/files/'+
-                                    data.context_info.conflicts.id+'/content',
-                            }, function(err, data) {
-                                if (err) {
-                                    node.error(RED._("box.error.upload-failed",{err:err.toString()}),msg);
-                                    node.status({fill:"red",shape:"ring",text:"box.status.failed"});
-                                    return;
-                                } else {
-                                    msg.payload = "https://app.box.com/notes/" + data.entries[0].id || ''; 
-                                    node.send(msg);                       
-                                }
-                                node.status({});
-                            });
-                            var form = r.form();
-                            if (localFilename) {
-                                form.append('filename', fs.createReadStream(localFilename), { filename: basename });
-                            } else {
-                                form.append('filename', RED.util.ensureBuffer(msg.payload), { filename: basename });
-                            }
-                        } else {
-                            node.error(RED._("box.error.upload-failed",{err:err.toString()}),msg);
-                            node.status({fill:"red",shape:"ring",text:"box.status.failed"});
-                        }
+                        node.error(RED._("box.error.path-resolve-failed",{err:err.toString()}),msg);
+                        node.status({fill:"red",shape:"ring",text:"box.status.failed"});
                         return;
+                    }
+                    node.status({fill:"blue",shape:"dot",text:"box.status.uploading"});                  
+                    var r = node.box.request({
+                        method: 'POST',
+                        url: 'https://upload.box.com/api/2.0/files/content'
+                    }, function(err, data) {                                      
+                        if (err) {
+                            if (data && data.status === 409 &&
+                                data.context_info && data.context_info.conflicts) {
+                                // existing file, attempt to overwrite it
+                                node.status({fill:"blue",shape:"dot",text:"box.status.overwriting"});
+                                var r = node.box.request({
+                                    method: 'POST',
+                                    url: 'https://upload.box.com/api/2.0/files/'+
+                                        data.context_info.conflicts.id+'/content',
+                                }, function(err, data) {
+                                    if (err) {
+                                        node.error(RED._("box.error.upload-failed",{err:err.toString()}),msg);
+                                        node.status({fill:"red",shape:"ring",text:"box.status.failed"});
+                                        return;
+                                    } else {
+                                        msg.payload = "https://app.box.com/notes/" + data.entries[0].id || ''; 
+                                        node.send(msg);                       
+                                    }
+                                    node.status({});
+                                });
+                                var form = r.form();
+                                if (localFilename) {
+                                    form.append('filename', fs.createReadStream(localFilename), { filename: basename });
+                                } else {
+                                    form.append('filename', RED.util.ensureBuffer(msg.payload), { filename: basename });
+                                }
+                            } else {
+                                node.error(RED._("box.error.upload-failed",{err:err.toString()}),msg);
+                                node.status({fill:"red",shape:"ring",text:"box.status.failed"});
+                            }
+                            return;
+                        } else {
+                            msg.payload = "https://app.box.com/notes/" + data.entries[0].id || ''; 
+                            node.send(msg);                       
+                        }                    
+                        node.status({});
+                    });
+                    var form = r.form();
+                    if (localFilename) {
+                        form.append('filename', fs.createReadStream(localFilename), { filename: basename });
                     } else {
-                        msg.payload = "https://app.box.com/notes/" + data.entries[0].id || ''; 
-                        node.send(msg);                       
-                    }                    
-                    node.status({});
+                        form.append('filename', RED.util.ensureBuffer(msg.payload), { filename: basename });
+                    }
+                    form.append('parent_id', parent_id);
                 });
-                var form = r.form();                
-                if (localFilename) {
-                    form.append('filename', fs.createReadStream(localFilename), { filename: basename });
-                } else {
-                    form.append('filename', RED.util.ensureBuffer(msg.payload), { filename: basename });
+                return;
+            }
+            var template = urlTemplate.split("/").pop()          
+                
+            var r = node.box.request({
+                method: 'POST',
+                url: 'https://api.box.com/2.0/files/' + template + '/copy',
+                body: {
+                    "name": filename,
+                    "parent": {
+                        "id": folder
+                    }
                 }
-                form.append('parent_id', parent_id);                    
-            });                   
+            }, function(err, data) {                   
+                if (err) {
+                    if (data && data.status === 409 &&
+                        data.context_info && data.context_info.conflicts) {
+                        // existing file, attempt to overwrite it
+                        node.status({fill:"blue",shape:"dot",text:"box.status.overwriting"});
+                        var r = node.box.request({
+                            method: 'POST',
+                            url: 'https://upload.box.com/api/2.0/files/'+
+                                data.context_info.conflicts.id+'/content',
+                        }, function(err, data) {
+                            if (err) {
+                                node.error(RED._("box.error.upload-failed",{err:err.toString()}),msg);
+                                node.status({fill:"red",shape:"ring",text:"box.status.failed"});
+                                return;
+                            } else {
+                                msg.payload = "https://app.box.com/notes/" + data.entries[0].id || ''; 
+                                node.send(msg);                       
+                            }
+                            node.status({});
+                        });
+                        var form = r.form();
+                        if (localFilename) {
+                            form.append('filename', fs.createReadStream(localFilename), { filename: basename });
+                        } else {
+                            form.append('filename', RED.util.ensureBuffer(msg.payload), { filename: basename });
+                        }
+                    } else {
+                        node.error(RED._("box.error.upload-failed",{err:err.toString()}),msg);
+                        node.status({fill:"red",shape:"ring",text:"box.status.failed"});
+                    }
+                    return;
+                } else {
+                    msg.payload = "https://app.box.com/notes/" + data.id || ''; 
+                    node.send(msg);                       
+                }                    
+                node.status({});
+            });                
         });
     }
-    RED.nodes.registerType("box out",BoxOutNode);
+    RED.nodes.registerType("box",BoxOutNode);
 };
